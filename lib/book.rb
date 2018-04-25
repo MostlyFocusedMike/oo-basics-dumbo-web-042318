150c1528b237742986b641bce9cf39dac053affb
Mike C <mikecronin92@gmail.com>
2018-04-25 00:11:10 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,15 +8,3 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 end
-
-class Shoe 
-  attr_accessor :brand, :color, :size, :material, :condition
-  def initialize(brand)
-    @brand = brand
-  end
-  
-  def cobble
-    puts "Your shoe is as good as new!"
-    @condition = "new"
-  end
-end
